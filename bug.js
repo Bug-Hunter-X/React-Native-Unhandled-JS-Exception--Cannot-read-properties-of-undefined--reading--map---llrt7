07e3edda120969e5dcc1f7100edd219aeec69004
BugHunter-X <askzenai@gmail.com>
2024-12-21 16:26:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a state variable before it has been initialized. This is a common mistake when working with asynchronous operations, such as fetching data from an API.
